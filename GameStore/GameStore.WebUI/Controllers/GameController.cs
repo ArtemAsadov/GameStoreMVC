cc6a35c4720b6f15d79d2d13899bbb3489ac92cb
Артем Асадов <kekc33rus@gmail.com>
2020-07-14 19:33:11 +0300

    [Dev] Разбиение на страницы

--- GameStore/GameStore.WebUI/Controllers/GameController.cs
+++ GameStore/GameStore.WebUI/Controllers/GameController.cs
@@ -10,12 +10,18 @@ namespace GameStore.WebUI.Controllers
     public class GameController : Controller
     {
         private IGameRepository repository;
-        public GameController(IGameRepository _repository) {
+        public int pageSize = 4; //Максимальное количество отоброжаемой информации на странице
+        public GameController(IGameRepository _repository)
+        {
             repository = _repository;
         }
 
-        public ViewResult List() {
-            return View(repository.Games);
+        public ViewResult List(int page = 1)
+        {
+            return View(repository.Games
+                .OrderBy(g => g.GameId) //Сортируем по ID ку
+                .Skip((page - 1) * pageSize) // Пропускаем столько элементов сколько было сумарно на предыдущих страницах
+                .Take(pageSize)); // Берем то что осталось в количестве pageSize
         }
     }
 }
